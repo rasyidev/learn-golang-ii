c0d464023c2c167f47a9374166c339f6fc00ebe3
rasyidev <habib.rasyid11@gmail.com>
2022-08-28 20:24:20 +0700

    new file:   10-deklarasi-banyak-constant.go new file:   9-constant.go

---
+++ 10-deklarasi-banyak-constant.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	const (
+		firstName = "Rasyidev"
+		lastName = "Pro"
+		wpm	= 32
+	)
+}
---
+++ 9-constant.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// constant tidak harus digunakan. Tidak akan error walaupun tidak digunakan
+	const PI = 3.14
+	const name = "Rasyidev"
+	fmt.Println("Nilai PI\t:", PI)
+}
