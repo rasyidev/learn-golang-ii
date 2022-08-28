3100ee40bf7178b52b846295721d81752a9638cb
rasyidev <habib.rasyid11@gmail.com>
2022-08-28 19:55:59 +0700

    new file:   5-function-len-dan-slicing-pada-string.go

---
+++ 5-function-len-dan-slicing-pada-string.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Total karakter pada teks ini adalah ", len("Total karakter pada teks ini adalah "))
+	fmt.Println("Nomor ASCII untuk karakter 'A' adalah: ", "A"[0])
+}
