0899a22b8b7312b1d223dcc9d2f4844b9e629c67
rasyidev <habib.rasyid11@gmail.com>
2022-08-30 08:57:44 +0700

    new file:   25-function.go new file:   26-function-parameter.go

---
+++ 25-function.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func printSeparator() {
+	fmt.Println("--------------------------------------------")
+}
+
+func introduce() {
+	fmt.Println("Hello my name is Rasyidev")
+}
+
+func main() {
+	introduce()
+	printSeparator()
+}
---
+++ 26-function-parameter.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func intro(nama string, kelas string) {
+	fmt.Println("Halo, nama saya", nama, "Saya dari kelas", kelas)
+}
+
+func main() {
+	intro("Rasyidev", "A")
+}
