5cb6f17bf46c6e268ba0db051075cc1e271e5382
rasyidev <habib.rasyid11@gmail.com>
2022-09-02 10:25:54 +0700

    new file:   database/database.go

---
+++ database/database.go
@@ -0,0 +1,11 @@
+package database
+
+import "fmt"
+
+func init() {
+	fmt.Println("Constructor atau package initialization dijalankan")
+}
+
+func Connect() {
+	fmt.Println("Koneksi database berhasil")
+}
