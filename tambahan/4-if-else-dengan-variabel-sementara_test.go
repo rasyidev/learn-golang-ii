80afa26764dd3f89fdf86c8a3d0547b5b1e56bd1
rasyidev <habib.rasyid11@gmail.com>
2022-09-21 19:56:11 +0700

    Tambah keterangan variabel sementara

--- tambahan/4-if-else-dengan-variabel-sementara_test.go
+++ tambahan/4-if-else-dengan-variabel-sementara_test.go
@@ -8,11 +8,18 @@ import (
 func TestIfElseVariabelTambahan(t *testing.T) {
 	currentYear := 2021
 
+	// age adalah variabel sementara
 	if age := currentYear - 1999; age < 17 {
 		fmt.Println("Belum boleh buat kartu sim")
 	} else {
 		fmt.Println("Sudah boleh buat kartu sim")
 	}
+
+	/*
+		baris code di bawah ini bisa bikin error
+		karena age cuma variabel sementara
+	*/
+	// fmt.Println("Age \t:", age)
 }
 
 /*
